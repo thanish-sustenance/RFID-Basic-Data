a99612e679d0fb39d67988484d89a291f7b04c49
thanish-sustenance <101925853+thanish-sustenance@users.noreply.github.com>
2022-03-21 00:24:32 +0530

    Create mqtt.ino

---
+++ mqtt trial/mqtt.ino
@@ -0,0 +1,57 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "AndroidAP2B58";
+const char* password =  "thanish13";
+const char* ID= "rfid";
+
+
+IPAddress broker(192,168,55,69); 
+WiFiClient wclient;
+
+PubSubClient client(wclient); 
+bool state=0;
+
+// Connect to WiFi network
+void setup_wifi() {
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password); 
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(ID)) {
+      Serial.println("connected");
+      Serial.println('\n');
+
+    } else {
+      Serial.println(" try again in 5 seconds");
+  
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200); 
+  delay(100);
+  setup_wifi(); 
+  client.setServer(broker, 1883);
+}
+
+void loop() {
+}
