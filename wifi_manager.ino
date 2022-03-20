ddab33c8cc66938bdc25497381ab120b694f3b66
thanish-sustenance <101925853+thanish-sustenance@users.noreply.github.com>
2022-03-21 00:01:43 +0530

    Update wifi_manager.ino

--- wifi_manager.ino
+++ wifi_manager.ino
@@ -1,21 +1,27 @@
+#include <WiFiManager.h> 
 
-
-#include <WiFiManager.h>
- 
 void setup() {
- 
+    WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+
     Serial.begin(115200);
-     
-    WiFiManager manager;    
-     
-    bool success = manager.autoConnect("ESP32_AP","password");
- 
-    if(!success) {
+
+    WiFiManager wm;
+
+    bool res;
+   
+    res = wm.autoConnect("AutoConnectAP","password"); 
+
+    if(!res) {
         Serial.println("Failed to connect");
+
     } 
     else {
-        Serial.println("Connected");
+      
+        Serial.println("connected.)");
     }
+
 }
- 
-void loop(){}
+
+void loop() {
+    // put your main code here, to run repeatedly:   
+}
